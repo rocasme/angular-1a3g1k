bbd889ca084cb24bc129a8568d3b2b8048b4886a
rocasme <96982753+rocasme@users.noreply.github.com>
2023-03-27 19:07:07 +0200

    Initial commit

---
+++ src/app/toolbar-multirow-example.ts
@@ -0,0 +1,16 @@
+import {Component} from '@angular/core';
+
+/**
+ * @title Multi-row toolbar
+ */
+@Component({
+  selector: 'toolbar-multirow-example',
+  templateUrl: 'toolbar-multirow-example.html',
+  styleUrls: ['toolbar-multirow-example.css'],
+})
+export class ToolbarMultirowExample {}
+
+
+/**  Copyright 2023 Google LLC. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at https://angular.io/license */
